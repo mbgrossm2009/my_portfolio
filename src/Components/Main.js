2c3044a56a802b5d02f5dee39007241087fa0fc5
Matt Grossman <grossmanmatt@Matts-MacBook-Pro.local>
2019-01-29 22:31:42 -0500

    first commit

---
+++ src/Components/Main.js
@@ -0,0 +1,21 @@
+
+import React, { Component } from "react";
+import Navbar from './Navbar'
+import Jumbotron from './Jumbotron'
+
+class Main extends Component {
+    render(){
+        return (
+            <div>
+        <Navbar/>
+   
+        <Jumbotron/>
+        </div>
+        )
+
+    }
+}
+
+
+
+export default Main;
