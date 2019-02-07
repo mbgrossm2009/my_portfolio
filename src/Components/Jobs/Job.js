19038a80f5548f3b1ca3eab323746ff182ad9d21
Matt Grossman <grossmanmatt@Matts-MBP.fios-router.home>
2019-02-06 19:40:30 -0500

    second picture

--- src/Components/Jobs/Job.js
+++ src/Components/Jobs/Job.js
@@ -1,41 +1,38 @@
-import React from "react";
+import React from 'react';
 
 class Job extends React.Component {
-  render() {
-    const { title, company, description, src } = this.props;
-    const listItems = description.map(item => {
-      return (
-        <div className="jobFunctions">
-          <li>{item}</li>
-        </div>
-      );
-    });
+	render() {
+		const { title, company, description, src } = this.props;
+		const listItems = description.map((item) => {
+			return (
+				<div className='jobFunctions'>
+					<li>{item}</li>
+				</div>
+			);
+		});
 
-    return (
-      <div>
-        <div className="container">
-          <div className="row">
-            <ul className="col-lg-6 job">
-              <li>
-                <h2>{company}</h2>
-              </li>
-              <li>
-                <h6>{title}</h6>
-              </li>
-              <li>{listItems}</li>
-            </ul>
-          </div>
-          <div className="row">
-            <ul>
-              <li>
-                <img className="col-lg-8 myImage" src={src} />
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    );
-  }
+		return (
+			<div>
+				<div className='row'>
+					<div className='col-lg-7'>
+						<ul className='job'>
+							<li>
+								<h2>{company}</h2>
+							</li>
+							<li>
+								<h6>{title}</h6>
+							</li>
+							<li>{listItems}</li>
+						</ul>
+					</div>
+
+					<li>
+						<img className='col-lg-5 myImage' src={src} />
+					</li>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Job;
