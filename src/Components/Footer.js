a6f3b2e742fb7760840a91ed0f4b922e465d9c99
Matt Grossman <grossmanmatt@Matts-MacBook-Pro.local>
2019-03-20 23:06:54 -0400

    master

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -16,7 +16,7 @@ class Footer extends Component {
 		 '"Whatever the mind of man can conceive and believe, it can achieve. –Napoleon Hill"',
 			'"Strive not to be a success, but rather to be of value. –Albert Einstein"',
 			'"You miss 100% of the shots you don’t take. –Wayne Gretzky\" -Michael Scott"',
-		 '" Every strike brings me closer to the next home run. –Babe Ruth"',
+		 '"Every strike brings me closer to the next home run. –Babe Ruth"',
 			'"Life isn\'t about getting and having, it\'s about giving and being. –Kevin Kruse"',
 			'"Life is what happens to you while you’re busy making other plans. –John Lennon"',
 			'"We become what we think about. –Earl Nightingale"',
