f7c625f8c317d979ed4360c0a2712bafa3b153c9
mbgrossm2009 <grossmanmatt@yahoo.com>
2023-07-17 14:54:57 -0400

    updated resume and professional experience

--- src/Components/ProfessionalExperience.js
+++ src/Components/ProfessionalExperience.js
@@ -13,8 +13,11 @@ class ProfessionalExperience extends Component {
           title="FreeLance Developer"
           company="Self Employed"
           description={[
-            "Design and develop websites based on clients specific needs",
-            "- Construct reusable components to be used in interactive UI kits"
+            "- Build, develop, and test customized websites to meet clients’ specific needs.",
+            "- Construct reusable components to be used in interactive UI kits",
+            "- Create reusable React components for interactive UI kits.",
+            "- Maintain and enhance existing websites with updated content.",
+            "- Stay current with emerging technologies and industry trends."
           ]}
           src={Matt2}
         />,
@@ -22,8 +25,9 @@ class ProfessionalExperience extends Component {
           title="Full Stack Web Developer"
           company="ME & FM Society of B.C."
           description={[
-            " Collaborated and developed a website, with a team, that allows patients to find the best doctor by searching a broad database.",
-            " - Allows patients to rate/comment these doctors based on their own experience."
+            "- Collaborated with a team to develop a search engine that helps patients find the most suitable physicians based on theirrequirements.",
+            "- Integrated a comment section allowing patients to engage and rate their experiences",
+            "- Technologies used: Ruby on Rails, HTML, CSS, Bootstrap, Heroku."
           ]}
           src={me1}
           url="https://sheltered-brushlands-68738.herokuapp.com/"
@@ -32,18 +36,14 @@ class ProfessionalExperience extends Component {
           title="Full Stack Web Developer"
           company="HathawayHomes-MA"
           description={[
-            " Designed and built a full-service real estate website with clean and organized code, in which users are able to customize their search based on a variety of categories such as geographical location, price point, number of rooms, and more. "
+            " - Designed and developed a full-service real estate website with clean and organized code, enabling users to customizetheir property search based on various criteria such as location, price point, and room count.",
+            "- Implemented a PostgreSQL database to store listings created by an Admin.",
+            "- Integrated the Zillow API using REST architecture.",
+            "- Installed and configured AWS and Heroku tools for development.",
+            " Technologies used: Ruby on Rails, HTML, CSS, Bootstrap, Heroku, AWS, Test Driven Development."
           ]}
           src={hathaway1}
           url="http://www.hathawayhomes-ma.com"
-        />,
-        <Job
-          title="Full Stack Web Developer"
-          company="Piece of Cake"
-          description={[
-            " Currently designing and building a website for a client looking to show off her creativity in the kitchen."
-          ]}
-					url = "https://github.com/mbgrossm2009/piece_of_cake"
         />
       ]
     };
